052a395ce927ae3edfb3eec04ce369fbd86ed95c
HopeWu <767088240@qq.com>
2022-06-26 23:17:48 +0800

    dp.py file created

---
+++ dp.py
@@ -0,0 +1,12 @@
+class Solution:
+    def lps(self, s: str) -> str:
+
+
+
+solve = Solution()
+solve.lps("abcdcab")
+solve.lps("abcdcba")
+solve.lps("abcd")
+solve.lps("abaadcd")
+solve.lps("workattech")
+solve.lps("workattaieng")
