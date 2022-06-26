ea865ed9b94d735d7b394d982ab3450a98d78ffe
HopeWu <767088240@qq.com>
2022-06-26 20:57:04 +0800

    brute force solution worked

--- brute_force.py
+++ brute_force.py
@@ -2,13 +2,20 @@ class Solution:
     def lps(self, s: str) -> str:
         n = len(s)
         dp = [0]*n
+        dp2 = [0]*n
         for i in range(1, n):
             j = 1
+            k = 0
             # while s[i - j] and s[i + j]:
-            while i - j > 0 and i + j < n:
+            while i - j >= 0 and i + j < n:
                 if s[i-j] == s[i+j]:
                     dp[i] += 1
                 j += 1
+
+            while i - k >= 0 and i + 1 + k < n:
+                if s[i-k] == s[i+1+k]:
+                    dp2[i] += 1
+                k += 1
         # return max(dp)
         # print(max(dp))
 
@@ -17,8 +24,21 @@ class Solution:
         for i in range(n):
             if dp[i] == _max:
                 _max_index = i
+                break
 
-        result = s[_max_index - _max: _max_index + _max]
+        _max_radius = max(dp2)
+        if _max_radius != 0:
+            _max_radius_index = 0
+            for i in range(n):
+                if dp2[i] == _max_radius:
+                    _max_radius_index = i
+                    break
+
+        if _max_index * 2 + 1 > _max_radius*2:
+            result = s[_max_index - _max: _max_index + 1 + _max]
+        else:
+            result = s[_max_radius_index + 1 - _max_radius: _max_radius_index + _max_radius + 1]
+
         print(result)
 
 
@@ -28,3 +48,5 @@ solve.lps("abcdcab")
 solve.lps("abcdcba")
 solve.lps("abcd")
 solve.lps("abaadcd")
+solve.lps("workattech")
+solve.lps("workattaieng")
