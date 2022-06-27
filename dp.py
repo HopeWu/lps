906472735fec86ef596c194185be7500703b1bfd
HopeWu <767088240@qq.com>
2022-06-27 12:45:37 +0800

    dp first version worked

--- dp.py
+++ dp.py
@@ -2,22 +2,43 @@ class Solution:
     def lps(self, s: str) -> str:
         n = len(s)
         dp = [1]*n
-        centers = [0]*n
-        flag = [1]*n
+        starts = [0]*n
+        ends = [0]*n
         for i in range(1, n):
             _max = 0
-            for j in range(i-1, int(i/2)):
-                k = 1
-                while j-k >= 0 and j+k < n:
-                    if s[j-k] == s[j+k]
+            for j in range(0, i):
+                k = 0
+                while(s[j+k] == s[i-k] and j+k < i-k):
+                    k += 1
+                if s[j+k] == s[i-k]:
+                    # got one
+                    if j+k == i-k:
+                        # odd length case
+                        if _max < k*2+1:
+                            _max = k*2+1
+                            starts[i] = j
+                            ends[i] = i
+                    if j+k == i-k+1:
+                        # even length case
+                        if _max < k*2:
+                            _max = k*2
+                            starts[i] = j
+                            ends[i] = i
+            if _max > dp[i-1]:
 
-            dp[i] = max(dp[i-1], dp[i-1] + 1)
+                dp[i] = _max
+            else:
+                starts[i] = starts[i-1]
+                ends[i] = ends[i-1]
+                dp[i] = dp[i-1]
+        print(dp[n-1])
+        return s[starts[n-1]: ends[n-1]+1]
 
 
 solve = Solution()
-solve.lps("abcdcab")
-solve.lps("abcdcba")
-solve.lps("abcd")
-solve.lps("abaadcd")
-solve.lps("workattech")
-solve.lps("workattaieng")
+print(solve.lps("abcdcab"))
+print(solve.lps("abcdcba"))
+print(solve.lps("abcd"))
+print(solve.lps("abaadcd"))
+print(solve.lps("workattech"))
+print(solve.lps("workattaieng"))
